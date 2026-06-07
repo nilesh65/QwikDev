6295249bc73b394654e2a3d52766c3e33e01be6e
niles <nileshmkjee123@gmail.com>
2026-06-07 16:12:14 +0530

    models and controllers created and routes are defined

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+const connectDb = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("DB is connected now")
+    }
+    catch(error){
+        console.log("DB error", error)
+    }
+}
+
+export default connectDb
--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 import express from  "express"
-
-import dotenv from dotenv
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 5000
+//middlewares
+app.use("/api/auth", authRouter)
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors(
+    {
+        origin:"http:localhost:5173",
+        credentials: true
+    }
+))
+
 
 app.listen(port,() => {
     console.log("server started");
+    connectDb()
 })
